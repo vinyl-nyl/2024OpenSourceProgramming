6544c0062d4dacbfa94e87d868c0df87d8ed7679
vinyl <junil2536@naver.com>
2024-09-27 12:24:22 +0900

    v.0.3 Type

--- week05/main.go
+++ week05/main.go
@@ -1,10 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"strings"
+)
 
 func main() {
-	var i int = 55
+
+	//var i int = 55
+	//i = 55
+
+	var f float32 = 4.30
+	i := 55
+
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i))
+	fmt.Printf("%s\n", strings.Title("kim inha"))
+	fmt.Println(math.Ceil(3.99))
+
 	fmt.Printf("value i : %d\n", i)
-	fmt.Printf("value i : ", i, "\n")
-	fmt.Printf("value i : ", i)
+	fmt.Print("value i : ", i, "\n")
+	fmt.Println("value i : ", i)
 }
