868e7023d29db1ef543b177e2d409c52ba3f9e96
vinyl <junil2536@naver.com>
2024-10-04 10:35:32 +0900

    v.0.4 Conversion

--- week06/main.go
+++ week06/main.go
@@ -5,14 +5,9 @@ import (
 )
 
 func main() {
-	var f float64
-	var i int
-	var b bool
-	var s string
+	i := 55
 
-	fmt.Println(f, b, s, i)
-	fmt.Printf("%f%t%s%d\n", f, b, s, i) // zero value
-	f = 2.7
-	i = 3
-	fmt.Print("\n\n", f > float64(i), "\n") // comparsion (true/false)
+	fmt.Printf("value i : %d\n", i)
+	fmt.Print("value i : ", i, "\n")
+	fmt.Println("value i : ", i)
 }
