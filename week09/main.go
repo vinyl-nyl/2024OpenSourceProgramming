8a0219c01f171aef28951ed9e337a7d79aba5a8a
vinyl <junil2536@naver.com>
2024-10-25 11:24:13 +0900

    v.1.9 Guess game

--- week09/main.go
+++ week09/main.go
@@ -1,13 +1,51 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
 	rand.Seed(time.Now().Unix())
-	target := rand.Intn(5) + 1 // print 0, 1, 2
-	fmt.Printf("%d\n", target)
+	answer := rand.Intn(6) + 1
+	fmt.Printf("%d\n", answer)
+
+	win := false
+	for guesses := 0; guesses < 3; guesses++ {
+		fmt.Printf("%d번의 기회가 남았습니다. 숫자 입력 : ", 3-guesses)
+		r := bufio.NewReader(os.Stdin)
+		i, err := r.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		i = strings.TrimSpace(i)
+		guess, err := strconv.Atoi(i)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(guess)
+
+		if answer == guess {
+			fmt.Println("정답입니다")
+			win = true
+			break
+		} else if answer > guess {
+			fmt.Println("입력하신 수는 정답보다 작은 수입니다. LOW")
+		} else {
+			fmt.Println("입력하신 수는 정답보다 큰 수입니다. HIGH")
+		}
+	}
+
+	if win {
+		fmt.Println("당신이 이겼습니다!")
+	} else {
+		fmt.Println("당신이 졌습니다.")
+	}
 }
