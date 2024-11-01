2a641a93e3fd7da844fea3cc5771d661c2e4f768
vinyl <junil2536@naver.com>
2024-11-01 11:55:20 +0900

    v.3.0 Prime number

--- week10/main.go
+++ week10/main.go
@@ -9,6 +9,23 @@ import (
 	"strings"
 )
 
+func isPrime(n int) bool { // 함수화, 소수면 true 소수가 아니면 false return
+	if n < 2 {
+		return false
+	} else if n == 2 {
+		return true
+	} else if n%2 == 0 {
+		return false
+	} else {
+		for j := 3; j*j <= n; j += 2 {
+			if n%j == 0 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
 func main() {
 	fmt.Print("점수입력 : ")
 	r := bufio.NewReader(os.Stdin)
@@ -20,28 +37,7 @@ func main() {
 	i = strings.TrimSpace(i)
 	n, err := strconv.Atoi(i)
 
-	// counts := 0
-	var isPrime bool = true // 가독성, 메모리 사이즈 개선
-
-	if n < 2 { // 1보다 큰 자연수 중 1과 자기 자신만을 약수로 가지는 수
-		isPrime = false
-	} else if n == 2 {
-		isPrime = true
-	} else if n%2 == 0 {
-		isPrime = false
-	} else {
-		for j := 3; j*j <= n; j += 2 {
-			if n%j == 0 { // 약수면
-				// counts++ // 나누어 떨어지는 횟수 카운트
-				isPrime = false
-				break
-			}
-			fmt.Printf("%d ", j) // 반복문 횟수 확인용 코드
-		}
-	}
-
-	// if counts == 0 { // 나누어 떨어지는 수가 있으면 안됨
-	if isPrime { // 비교 연산 제거
+	if isPrime(n) {
 		fmt.Printf("%d는(은) 소수입니다.", n)
 	} else {
 		fmt.Printf("%d는(은) 소수가 아닙니다.", n)
