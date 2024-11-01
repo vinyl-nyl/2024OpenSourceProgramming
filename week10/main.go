e40035a6e600ae5da70dea76359e121e89995cf6
vinyl <junil2536@naver.com>
2024-11-01 10:51:45 +0900

    v.2.4 Prime Number

--- week10/main.go
+++ week10/main.go
@@ -20,15 +20,17 @@ func main() {
 	i = strings.TrimSpace(i)
 	n, err := strconv.Atoi(i)
 
-	counts := 0
-
-	for j := 1; j <= n; j++ { // 1부터 입력된 수까지 반복
+	// counts := 0
+	var isPrime bool = true  // 가독성, 메모리 사이즈 개선
+	for j := 2; j < n; j++ { // 1부터 입력된 수까지 반복
 		if n%j == 0 { // 약수면
-			counts++ // 나누어 떨어지는 횟수 카운트
+			// counts++ // 나누어 떨어지는 횟수 카운트
+			isPrime = false
 		}
 	}
 
-	if counts == 2 {
+	// if counts == 0 { // 나누어 떨어지는 수가 있으면 안됨
+	if isPrime { // 비교 연산 제거
 		fmt.Printf("%d는(은) 소수입니다.", n)
 	} else {
 		fmt.Printf("%d는(은) 소수가 아닙니다.", n)
