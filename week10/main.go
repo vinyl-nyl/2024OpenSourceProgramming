0e4dc9f31298802938aa02cf9250a5725a87b26c
vinyl <junil2536@naver.com>
2024-11-01 11:37:18 +0900

    v.2.8 Prime number

--- week10/main.go
+++ week10/main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"log"
-	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -27,7 +26,8 @@ func main() {
 	if n < 2 { // 1보다 큰 자연수 중 1과 자기 자신만을 약수로 가지는 수
 		isPrime = false
 	} else {
-		for j := 2; j <= int(math.Sqrt(float64(n))); j++ { // 1부터 입력된 수까지 반복
+		// for j := 2; j <= int(math.Sqrt(float64(n))); j++ { // 1부터 입력된 수까지 반복
+		for j := 2; j*j <= n; j++ {
 			if n%j == 0 { // 약수면
 				// counts++ // 나누어 떨어지는 횟수 카운트
 				isPrime = false
